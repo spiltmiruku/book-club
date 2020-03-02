1dfc855fc477c7813da3b0476c52b8e0b138fa3e
spiltmiruku <spiltmiruku@gmail.com>
2020-03-02 01:38:14 -0700

    Tweaked login button styling;

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
+import bookstack from '../../resources/bookstack.svg';
+import location from '../../resources/location.svg';
+import thumbsup from '../../resources/thumbsup.svg';
+
 
 const Header = () => {
     return (
@@ -9,9 +13,18 @@ const Header = () => {
     <section className='menu-bar'>
         <h1 className='title'>Book Club</h1>
         <Link to='/'><span>Home</span></Link>
-        <div className='menu-select'>Previous Books</div>
-        <div className='menu-select'>Nominate a Book</div>
-        <div className='menu-select'>Host a Meeting</div>
+        <div className='menu-select'>
+        <img id="menu-icon" src={bookstack} alt="stack of books icon" />
+            Previous Books
+            </div>
+        <div className='menu-select'>
+        <img id="menu-icon" src={location} alt="location icon" />
+            Nominate a Book
+            </div>
+        <div className='menu-select'>
+        <img id="menu-icon" src={thumbsup} alt="thumbs up icon" />
+            Host a Meeting
+            </div>
 
         <div className='login'>Login</div>
     </section>
