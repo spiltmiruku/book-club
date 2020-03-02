6c9f4043671d692f4cb387e8d42ef5a4c7e77fbb
spiltmiruku <spiltmiruku@gmail.com>
2020-03-01 20:50:57 -0700

    Started initial styling of book-display section; Trying neumorphism in UI;

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import './landing.css';
-import teachustooutgrow from '../../resources/teachustooutgrow.jpg';
+import React from "react";
+import "./landing.css";
+import teachustooutgrow from "../../resources/teachustooutgrow.jpg";
 
 const Landing = () => {
-   return (
+  return (
     <div>
-        <h1 className='tag'>[READ // GATHER // DISCUSS // REPEAT]</h1>
-    <section className='current-book'>
-        <div>
-            <img id='book-cover' src={teachustooutgrow} alt='book cover' />
-        </div>
-    </section>
+      <h1 className="tag">[READ // GATHER // DISCUSS // REPEAT]</h1>
+      <div className="landing-display">
+        <section className="current-book">
+        <h2>book of the month</h2>
 
-    <section className='meeting-info'>
-        <div className='meet-date-time'>
-            Upcoming Meeting:
-        </div>
-        <div className='discussion-leader'>
-            Discussion Leader:
-        </div>
-        <div className='next-pick'>
-            Next Pick:
-        </div>
-    </section>
-</div>
-   )
-}
+            <img id="book-cover" src={teachustooutgrow} alt="book cover" />
+          <div className="book-display">
+          </div>
+            <p>March 2020</p>
+        </section>
+
+        <section className="meeting-info">
+          <div className="meet-date-time">Upcoming Meeting:</div>
+          <div className="discussion-leader">Discussion Leader:</div>
+          <div className="next-pick">Next Pick:</div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
