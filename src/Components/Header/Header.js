813b09384122660533fd6c89aee192082b6ee496
spiltmiruku <spiltmiruku@gmail.com>
2020-08-31 22:22:44 -0500

    Add new Component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './header.css';
-import bookstack from '../../resources/bookstack.svg';
-import location from '../../resources/location.svg';
-import thumbsup from '../../resources/thumbsup.svg';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
+import bookstack from "../../resources/bookstack.svg";
+import location from "../../resources/location.svg";
+import thumbsup from "../../resources/thumbsup.svg";
 
 const Header = () => {
-    return (
-<div className='side-menu'>
-
-    <section className='menu-bar'>
-    {/* <Link to='/'> </Link> */}
-    <h1 className='title'>Book Club</h1>
-        {/* <div className='menu-select'>
-        <img id="menu-icon" src={bookstack} alt="stack of books icon" />
-            Previous Books
-            </div>
+  return (
+    <div className="side-menu">
+      <section className="menu-bar">
+        <Link to="/"> HOME </Link>
+        <h1 className="title">Book Club</h1>
+        <div className="menu-select">
+          <img id="menu-icon" src={bookstack} alt="stack of books icon" />
+          Previous Books
+        </div>
+        {/* 
         <div className='menu-select'>
         <img id="menu-icon" src={location} alt="location icon" />
             Nominate a Book
@@ -27,9 +26,9 @@ const Header = () => {
             </div>
 
         <div className='login'>Login</div> */}
-    </section>
-</div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Landing from './Components/Landing/Landing';
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Landing from "./Components/Landing/Landing";
 
 export default (
-    <Switch>
-        <Route exact path='/' component={Landing} />
-        {/* <Route path='/previousbooks' component={Projects} />
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route path="/previousbooks" component={History} />
+    {/* 
         <Route path='/nominate' component={About} />
-        <Route path='/hostmeeting' component={Contact} /> */}
-    </Switch>
+        <Route path='/hostmeeting' component={Contact} /> 
+        */}
+  </Switch>
 );
