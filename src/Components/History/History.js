4842f47beb3a981ed9547d52965a7015a3e65b84
spiltmiruku <spiltmiruku@gmail.com>
2020-08-31 22:26:04 -0500

    Add route

---
+++ src/Components/History/History.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./history.css";
+
+const History = () => {
+  return <main>
+      
+  </main>;
+};
+
+export default History;
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
+import History from "./Components/History/History";
 
 export default (
   <Switch>
     <Route exact path="/" component={Landing} />
     <Route path="/previousbooks" component={History} />
-    {/* 
-        <Route path='/nominate' component={About} />
-        <Route path='/hostmeeting' component={Contact} /> 
-        */}
+    {/* <Route path='/nominate' component={About} />
+        <Route path='/hostmeeting' component={Contact} /> */}
   </Switch>
 );
