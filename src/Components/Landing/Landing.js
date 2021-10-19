dc5bf2ef5341a17020c3b8ce3f2fe65be6d9f82f
spiltmiruku <spiltmiruku@gmail.com>
2021-10-19 10:52:12 -0600

    update meeting day

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -4,11 +4,11 @@ import currentbook from "../../resources/currentbook.jpg";
 
 const Landing = () => {
   let meetingPlace = `3:00PM MST @ discord`;
-  let meetingDateTime = "Sunday, Oct 31st, 2021";
+  let meetingDateTime = "Sunday, Nov 7th, 2021";
   let bookMonth = "October 2021";
   let discussionLeader = "Trenton";
   let nextPick = "Catherine";
-``
+
   return (
     <div>
       <h1 className="tag">[READ // GATHER // DISCUSS // REPEAT]</h1>
